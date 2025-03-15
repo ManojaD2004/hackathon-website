4fa851500a3355d0f7c9a69c1aa588358e71a941
Vilas-cp <vilaspgowda1000@gmail.com>
2025-03-15 15:39:29 +0530

    fontadded

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div className="bg-[#fff5ee] h-screen w-full selection:bg-[#f13f3e] selection:text-[#fff5ee]">
+      <div className="text-center  text-4xl text-[#f13f3e]">
+        <p className="font-extrabold">
+          Problem<br></br> statements
+        </p>
+      </div>
+      <p className="text-[#f13f3e] text-9xl font-extrabold ">
+        <span>Digital solutions <br></br>for rural education.</span>
+      </p>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
